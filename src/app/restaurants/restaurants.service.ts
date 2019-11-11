4faa7e0ecd776d4ab6924940824cc1321043fe1b
Matheus Rocha <professormatheusvieira@gmail.com>
2019-11-11 00:22:37 -0300

    6 commit

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,6 +4,8 @@ import { Http } from "@angular/http";
 import {MEAT_API} from '../app.api'
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+import {ErrorHandler} from "../app.error-handler"
 
 @Injectable()
 export class RestaurantsService {
@@ -11,7 +13,7 @@ export class RestaurantsService {
     constructor(private http: Http){}
 
     restaurants(): Observable<Restaurant[]> {
-        return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+        return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json()).catch(ErrorHandler.handleError)
     }
         
    
