d3fdb96f38e1de9b4fc521518c7e46356dfad6cc
Matheus Rocha <professormatheusvieira@gmail.com>
2019-11-12 02:05:47 -0300

    10 commit

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 import {ErrorHandler} from "../app.error-handler"
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
 
 @Injectable()
 export class RestaurantsService {
@@ -22,6 +23,10 @@ export class RestaurantsService {
 
     reviewsOfRestaurant(id: String): Observable<any>{
         return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`).map(response => response.json()).catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`).map(response => response.json()).catch(ErrorHandler.handleError)
     }
 
     
