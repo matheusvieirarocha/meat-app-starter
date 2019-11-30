f71cff9a7956568413248bf96ed7a92da662c3b2
Matheus Rocha <professormatheusvieira@gmail.com>
2019-11-29 23:46:47 -0300

    17 commit

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit} from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
+import { OrderService } from './order.service';
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+
+
 
 @Component({
   selector: 'mt-order',
@@ -7,10 +12,38 @@ import { Component, OnInit} from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  delivery: number = 8
+  paymentOptions: RadioOption[]= [
+    {label: 'Dinheiro', value: 'MON'},
+    {label: 'Cartao de Debito', value: 'DEB'},
+    {label: 'Cartao Refeicao', value: 'REF'}
+  ]
+
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
   }
+
+  itemsValue(): number{
+    return this.orderService.itemsValue()
+  }
+  cartItems(): CartItem[]{
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem){
+    this.orderService.increaseQty(item)
+  }
+
+  decreaseQty(item: CartItem){
+    this.orderService.decreaseQty(item)
+  }
+
+  remove(item: CartItem){
+    this.orderService.remove(item)
+  }
+
+
 
  
 
--- src/app/restaurant-detail/shopping-cart/shopping-cart-service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart-service.ts
@@ -12,7 +12,7 @@ export class ShoppingCartService {
     addItem(item : MenuItem){
         let foundItem = this.items.find((mItem)=>mItem.menuItem.id === item.id)
         if(foundItem){
-            foundItem.qtd += 1
+            this.increaseQty(foundItem)
         }else{
             this.items.push(new CartItem(item))
         }
@@ -22,7 +22,20 @@ export class ShoppingCartService {
         this.items.splice(this.items.indexOf(item), 1)
     }
 
+    increaseQty(item: CartItem){
+        item.qtd = item.qtd + 1
+    }
+
+    decreaseQty(item: CartItem){
+        item.qtd -= 1
+        if(item.qtd === 0){
+            this.removeItem(item)
+        }
+    }
+
     total(): number{
         return this.items.map(item => item.value()).reduce((prev, value)=> prev+value, 0)
     }
+
+
 }
